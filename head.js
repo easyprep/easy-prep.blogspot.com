0844e894ca68ec4cf62249c8248e08d008b47816
easyprep <nka.easyprep@gmail.com>
2021-01-20 03:44:33 +0000

    update

--- head.js
+++ head.js
@@ -1,4 +1,6 @@
 console.log('head.js');
+const m = parseInt(location.search.split(/[?&]/).filter(a=>a.split('=')[0]=='m').map(a=>a.split('=')[1]).join());
+console.log(m);
 
 // Constants
 const shortTermCache = 1; //minute
@@ -12,34 +14,26 @@ const $ = function (selector, doc = document) {
 
 // Events
 window.onload = function (e) {
-    let cahce = cacheDocument(document, location);
-    window.history.pushState(cache, cache.title, cache.path);
+    let cahce = parseContent(document);
+    window.history.pushState(cache, cache.title, cache.href);
 }
 
 window.onpopstate = function (e) {
-    showContent(e.state, false);
+    showContent(e.state);
 }
 
 // Common Functions
 function GoUsingAjax(a) {
+    
+    if (location.href == a.href) return;
 
-    if (location.pathname == a.pathname) return;
-
-    let cache = localStorage['cache:/' + a.pathname];
+    let cache = getCache(a);
 
     if (cache) {
-        cache = JSON.parse(cache);
-
-        showContent(cache, true);
-
-        console.log('From Cache : ', a.pathname);
-
-        if ((Date.now() - cache.ts) > (cache.expiry * 60000)) {
-            loadFromServer(a);
-        }
-
+        showContent(cache);
+        window.history.pushState(cache, cache.title, cache.href);
+        console.log('From Cache : ', a.href);
     } else {
-
         loadFromServer(a);
     }
 }
@@ -55,42 +49,50 @@ function loadFromServer(a) {
         let doc = document.createElement('html');
         doc.innerHTML = html;
 
-        let cache = cacheDocument(doc, a);
+        let cache = parseContent(doc,a);
+        setCache(cache);
+        showContent(cache);
 
-        showContent(cache, true);
-
-        console.log('From Server : ', a.pathname);
-
-    }).catch();
+        console.log('From Server : ', a.href);
+    }).catch(e => {
+        $('#loading').style.display = "none";
+        console.log(e);
+    });
 }
 
-function cacheDocument(doc, a) {
-
+function parseContent(doc) {
     let htm = $('#app', doc).innerHTML;
     let title = $('title', doc)[0].text;
-    let expiry = (parseInt(localStorage.longTermCache) || longTermCache);
-
-    if (a.pathname == '/' || a.pathname.indexOf('/search/') == 0) {
-        expiry = shortTermCache;
-    }
-
-    cache = { htm, title, expiry, ts: Date.now(), path: a.pathname };
-
-    if (!!a.search && a.pathname == '/search') {
-        cache.path = a.pathname + a.search + a.hash;
-    } else {
-        localStorage['cache:/' + a.pathname] = JSON.stringify(cache);
-    }
-
-    return cache;
+    let href = a.href;
+    return { htm, title, href };
 }
 
-function showContent(cache, pushHistory) {
-
+function showContent(cache) {
     $('#app').innerHTML = cache.htm;
     document.title = cache.title;
+}
 
-    if (pushHistory) {
-        window.history.pushState(cache, cache.title, cache.path);
+function getCache(a) {
+    let s = a.href.indexOf('.html') != -1 ? localStorage : sessionStorage;
+    return s[a.href] ? JSON.parse(s[a.href]) : null;
+}
+
+function setCache(cache) {
+    let s = cache.href.indexOf('.html') != -1 ? localStorage : sessionStorage;
+    try {
+        s[cache.href] = JSON.stringify(cache);
+    } catch (e) {
+        console.log(e);
     }
 }
+
+//Extra
+    // if (a.pathname == '/' || a.pathname.indexOf('/search/') == 0) {
+    //     expiry = shortTermCache;
+    // }
+
+    // if (!!a.search && a.pathname == '/search') {
+    //     cache.path = a.pathname + a.search + a.hash;
+    // } else {
+    //     localStorage['cache:/' + a.pathname] = JSON.stringify(cache);
+    // }
