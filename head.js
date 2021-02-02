650ed2a0430e69511f12ae5ce4c7ed27dfb24c8b
easyprep <nka.easyprep@gmail.com>
2021-02-02 21:52:03 +0530

    update

--- head.js
+++ head.js
@@ -59,7 +59,7 @@ window.onpopstate = (e) => {
 
 // Common Functions
 function GoUsingAjax(event) {
-  console.log(event);
+  //console.log(event);
 
   event.preventDefault();
 
@@ -90,7 +90,7 @@ function GoUsingAjax(event) {
       window.history.pushState(cache, cache.title, a.href);
       console.log('From Cache : ', a.href);
       $('#main').style.opacity = 1;
-    }, 200);
+    }, 300);
     // if(Date.now()-cache.ts > 60*1000){
     //     prependChild('<p class="cache-ts">Page is ' + getCacheAge(cache.ts).filter(item=>item.value).map(item=>`<b>${item.value}</b> ${item.key}`).join(' ') + ' old. Refresh to get latest.</p>', $('#main'));
     // }
