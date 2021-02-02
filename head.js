8d48b8c1103d1ef3a8132e64b6f12a4f4bf3a6fb
easyprep <nka.easyprep@gmail.com>
2021-02-02 20:48:52 +0530

    update

--- head.js
+++ head.js
@@ -2,164 +2,186 @@ console.log('head.js');
 
 // Query String
 const qs = {
-    stringify: (json) => {
-        let qs =
-            Object.keys(json).map(function (key) {
-                return encodeURIComponent(key) + '=' +
-                    encodeURIComponent(json[key]);
-            }).join('&');
-        return qs ? '?' + qs : '';
-    },
-    parse: (str) => {
-        let json = {};
-        str.split(/[?&]/).filter((a, i) => i).forEach(a => {
-            let f = a.split('=');
-            json[f[0]] = f[1];
-        });
-        return json;
-    }
-}
+  stringify: (json) => {
+    let qs = Object.keys(json)
+      .map(function (key) {
+        return encodeURIComponent(key) + '=' + encodeURIComponent(json[key]);
+      })
+      .join('&');
+    return qs ? '?' + qs : '';
+  },
+  parse: (str) => {
+    let json = {};
+    str
+      .split(/[?&]/)
+      .filter((a, i) => i)
+      .forEach((a) => {
+        let f = a.split('=');
+        json[f[0]] = f[1];
+      });
+    return json;
+  },
+};
 
 // Dom Selector
 const $ = function (selector, doc = document) {
-    return selector.split(/[ >]/).pop().indexOf('#') == 0 ? doc.querySelector(selector) : doc.querySelectorAll(selector);
-}
+  return selector.split(/[ >]/).pop().indexOf('#') == 0
+    ? doc.querySelector(selector)
+    : doc.querySelectorAll(selector);
+};
 
 const appendChild = (html, parent) => {
-    let child = document.createElement('div');
-    child.innerHTML = html;
-    parent.appendChild(child);
-}
+  let child = document.createElement('div');
+  child.innerHTML = html;
+  parent.appendChild(child);
+};
 
 const prependChild = (html, parent) => {
-    let child = document.createElement('div');
-    child.innerHTML = html;
-    parent.insertBefore(child, parent.firstChild);
-}
+  let child = document.createElement('div');
+  child.innerHTML = html;
+  parent.insertBefore(child, parent.firstChild);
+};
 
 //Main
 window.qObj = qs.parse(location.search);
 
 // Events
 window.onload = (e) => {
-    let cache = parseContent(document);
-    setCache(location, cache);
-    window.history.pushState(cache, cache.title, location.href);
-}
+  let cache = parseContent(document);
+  setCache(location, cache);
+  window.history.pushState(cache, cache.title, location.href);
+};
 
 window.onpopstate = (e) => {
-    showContent(e.state);
-}
+  showContent(e.state);
+};
 
 // Common Functions
-function GoUsingAjax(a) {
+function GoUsingAjax(event) {
+  event.preventDefault();
 
-    if (a.nodeName == 'FORM') {
-        let url = a.action + '?' + new URLSearchParams(new FormData(a)).toString();
-        a = document.createElement('a');
-        a.href = url;
-    }
+  let a = event.target;
 
-    if (window.qObj.m) {
-        let qObj = qs.parse(a.search);
-        qObj.m = window.qObj.m;
-        a.search = qs.stringify(qObj);
-    }
+  if (a.nodeName == 'FORM') {
+    let url = a.action + '?' + new URLSearchParams(new FormData(a)).toString();
+    a = document.createElement('a');
+    a.href = url;
+  }
 
-    if (location.href == a.href) return;
+  if (window.qObj.m) {
+    let qObj = qs.parse(a.search);
+    qObj.m = window.qObj.m;
+    a.search = qs.stringify(qObj);
+  }
 
-    let cache = getCache(a);
+  if (location.href == a.href) return;
 
-    if (cache) {
-        showContent(cache);
-        window.history.pushState(cache, cache.title, a.href);
-        console.log('From Cache : ', a.href);
-        // if(Date.now()-cache.ts > 60*1000){
-        //     prependChild('<p class="cache-ts">Page is ' + getCacheAge(cache.ts).filter(item=>item.value).map(item=>`<b>${item.value}</b> ${item.key}`).join(' ') + ' old. Refresh to get latest.</p>', $('#main'));
-        // }
-    } else {
-        loadFromServer(a);
-    }
+  let cache = getCache(a);
+
+  if (cache) {
+    showContent(cache);
+    window.history.pushState(cache, cache.title, a.href);
+    console.log('From Cache : ', a.href);
+    // if(Date.now()-cache.ts > 60*1000){
+    //     prependChild('<p class="cache-ts">Page is ' + getCacheAge(cache.ts).filter(item=>item.value).map(item=>`<b>${item.value}</b> ${item.key}`).join(' ') + ' old. Refresh to get latest.</p>', $('#main'));
+    // }
+  } else {
+    loadFromServer(a);
+  }
 }
 
 function loadFromServer(a) {
+  $('#loading').style.display = '';
 
-    $('#loading').style.display = "";
+  fetch(a.href)
+    .then((res) => res.text())
+    .then((html) => {
+      $('#loading').style.display = 'none';
 
-    fetch(a.href).then(res => res.text()).then(html => {
+      let doc = document.createElement('html');
+      doc.innerHTML = html;
 
-        $('#loading').style.display = "none";
+      let cache = parseContent(doc);
+      setCache(a, cache);
+      showContent(cache);
+      window.history.pushState(cache, cache.title, a.href);
 
-        let doc = document.createElement('html');
-        doc.innerHTML = html;
-
-        let cache = parseContent(doc);
-        setCache(a, cache);
-        showContent(cache);
-        window.history.pushState(cache, cache.title, a.href);
-
-        console.log('From Server : ', a.href);
-    }).catch(e => {
-        $('#loading').style.display = "none";
-        console.log(e);
+      console.log('From Server : ', a.href);
+    })
+    .catch((e) => {
+      $('#loading').style.display = 'none';
+      console.log(e);
     });
 }
 
 function parseContent(doc) {
-    let content = $('#app', doc).innerHTML;
-    let title = $('title', doc)[0].text;
-    return { title, content };
+  let content = $('#app', doc).innerHTML;
+  let title = $('title', doc)[0].text;
+  return { title, content };
 }
 
 function showContent(cache) {
-    $('#app').innerHTML = cache.content;
-    document.title = cache.title;
+  document.title = cache.title;
+  $('#app').innerHTML = cache.content;
+  attachEventListener();
+}
+
+function attachEventListener() {
+  $('#app a').forEach(function (a) {
+    a.removeEventListener('click', ajaxRequest);
+    a.addEventListener('click', ajaxRequest);
+  });
+  $('#app form').forEach(function (a) {
+    a.removeEventListener('submit', ajaxRequest);
+    a.addEventListener('submit', ajaxRequest);
+  });
 }
 
 function getCache(a) {
-    let path = a.pathname + removeQueryParam(a.search, ['m']);
-    let storage = path.indexOf('.html') != -1 ? localStorage : sessionStorage;
-    return storage[path] ? JSON.parse(storage[path]) : null;
+  let path = a.pathname + removeQueryParam(a.search, ['m']);
+  let storage = path.indexOf('.html') != -1 ? localStorage : sessionStorage;
+  return storage[path] ? JSON.parse(storage[path]) : null;
 }
 
 function setCache(a, cache) {
-    let path = a.pathname + removeQueryParam(a.search, ['m']);
-    let storage = path.indexOf('.html') != -1 ? localStorage : sessionStorage;
-    try {
-        storage[path] = JSON.stringify({ ts: Date.now(), ...cache });
-    } catch (e) {
-        console.log(e);
-    }
+  let path = a.pathname + removeQueryParam(a.search, ['m']);
+  let storage = path.indexOf('.html') != -1 ? localStorage : sessionStorage;
+  try {
+    storage[path] = JSON.stringify({ ts: Date.now(), ...cache });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 function removeQueryParam(str, params) {
-    let qObj = qs.parse(str);
-    params.forEach(p => {
-        delete qObj[p];
-    });
-    return qs.stringify(qObj);
+  let qObj = qs.parse(str);
+  params.forEach((p) => {
+    delete qObj[p];
+  });
+  return qs.stringify(qObj);
 }
 
 function getCacheAge(ts) {
-    let d = Math.abs(Date.now() - ts) / 1000;   // delta
-    let r = [];                                 // result
-    let s = {                                   // structure
-        year: 31536000,
-        month: 2592000,
-        week: 604800,                           // uncomment row to ignore
-        day: 86400,                             // feel free to add your own row
-        hour: 3600,
-        minute: 60,
-        second: 1
-    };
+  let d = Math.abs(Date.now() - ts) / 1000; // delta
+  let r = []; // result
+  let s = {
+    // structure
+    year: 31536000,
+    month: 2592000,
+    week: 604800, // uncomment row to ignore
+    day: 86400, // feel free to add your own row
+    hour: 3600,
+    minute: 60,
+    second: 1,
+  };
 
-    Object.keys(s).forEach(function (key) {
-        let item = {key};
-        item.value = Math.floor(d / s[key]);
-        if(item.value>1)item.key += 's';
-        r.push(item);
-        d -= item.value * s[key];
-    });
+  Object.keys(s).forEach(function (key) {
+    let item = { key };
+    item.value = Math.floor(d / s[key]);
+    if (item.value > 1) item.key += 's';
+    r.push(item);
+    d -= item.value * s[key];
+  });
 
-    return r;
+  return r;
 }
